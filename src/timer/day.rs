9f5445ee876bfc7732ff24441f88221445f6faea
julius-boettger <julius.btg@proton.me>
2025-06-02 11:47:35 +0200

    feat: implement new for timers

--- src/timer/day.rs
+++ src/timer/day.rs
@@ -7,3 +7,11 @@ pub struct Timer {
     /// time to turn the plug off
     off_time: Time,
 }
+
+impl Timer {
+    /// with times to turn the plug on/off
+    fn new(on_time: Time, off_time: Time) -> Self {
+        assert_ne!(on_time, off_time);
+        Self { on_time, off_time }
+    }
+}
--- src/timer/year.rs
+++ src/timer/year.rs
@@ -5,3 +5,11 @@ pub struct Timer {
     /// does not include leap day
     day_timers: [day::Timer; 365]
 }
+
+impl Timer {
+    /// day timers don't include leap day
+    #[allow(clippy::large_types_passed_by_value)]
+    const fn new(day_timers: [day::Timer; 365]) -> Self {
+        Self { day_timers }
+    }
+}
