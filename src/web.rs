e22a896702f5fd4112575d30253a04aecb2c6d9e
julius-boettger <julius.btg@proton.me>
2025-06-23 10:01:02 +0200

    feat: allow cors for port 4173

--- src/web.rs
+++ src/web.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 use tower_http::cors::CorsLayer;
-use axum::{extract, Json, http::{header, HeaderValue, StatusCode, Method}};
+use axum::{extract, Json, http::{header, StatusCode, Method}};
 
 use crate::plug::Plug;
 use crate::sunrise_api;
@@ -312,7 +312,10 @@ pub async fn start_server(state: StateWrapper) {
 
         // allow CORS from frontend
         .layer(CorsLayer::new()
-            .allow_origin("http://localhost:5173".parse::<HeaderValue>().unwrap())
+            .allow_origin([
+                "http://localhost:4173".parse().unwrap(), // vite dev default
+                "http://localhost:5173".parse().unwrap(), // vite preview default
+            ])
             .allow_methods([Method::GET, Method::PUT])
             .allow_headers([header::CONTENT_TYPE]))
 
